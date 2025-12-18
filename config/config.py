4068a391e463f213ef5d8a06383f26189179c05b
felix <felix.noble@neuro-genesis.uk>
2025-12-18 14:10:30 +0000

    registry name updated

--- config/config.py
+++ config/config.py
@@ -6,13 +6,18 @@ class ModelConfig:
     encoder_layers: int = 5
 
 @dataclass(frozen=True)
+class DataConfig:
+    raw: Path = Path('/home/fnoble/data/OpenAlex-parquet/')
+    staged: Path = Path('/home/fnoble/data/staged/')
+
+@dataclass(frozen=True)
 class LogConfig:
     file: str = 'ERROR'
     console: str = 'DEBUG'
 
 @dataclass(frozen=True)
 class Config:
-    data_path = Path('/home/fnoble/data/')
+    data: DataConfig = DataConfig()
     model: ModelConfig = ModelConfig() 
     logging: LogConfig = LogConfig()
 
