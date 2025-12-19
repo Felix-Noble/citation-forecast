8e34763489c3c314449c694e5dc71799aff0f94e
felix <felix.noble@neuro-genesis.uk>
2025-12-19 07:36:47 +0000

    padding/truncation to dataloader

--- src/dataloaders/df_dataloader.py
+++ src/dataloaders/df_dataloader.py
@@ -1,7 +1,6 @@
-# ypyright: ignore[reportUnknownMemberType]
-# type: ignore
 import dask.dataframe as dd
 import pandas as pd # pyright: ignore[reportMissingTypeStubs] 
+import torch.nn as nn
 from torch import Tensor
 from torch.utils.data import Dataset
 from typing import cast, override
@@ -9,20 +8,41 @@ from typing import cast, override
 class DF_DataLoader(Dataset[tuple[Tensor, ...]]):
     def __init__(self, 
                  data_path: str,
-                 X: list[str],
-                 y: list[str] | None=None,
+                 X: str,
+                 y: str | None,
+                 max_len: int,
+                 pad_value: int = 0,
+                 pad: bool = True,
+                 truncate: bool = True,
                  ):
         super().__init__()
-        if y is None:
-            y = []
 
-        columns = list(set(X + y))
-        self.df: pd.DataFrame = cast(pd.DataFrame, dd.read_parquet(data_path, columns=columns).compute()) # pyright: ignore[reportPrivateImportUsage]
-        self.X: list[str] = X
-        self.y: list[str] = y
+        if y is None:
+            columns = [X]
+        else:
+            columns = list(set([X, y]))
+
+        self.df: pd.DataFrame = cast(pd.DataFrame, dd.read_parquet(data_path, columns=columns).compute()) # pyright: ignore[reportPrivateImportUsage, reportUnknownMemberType]
+        self.X: str = X
+        self.Y: str | None = y
+
+        self.PAD: bool = pad
+        self.TRUNCATE: bool = truncate
+        self.MAX_LEN: int = max_len
+        self.PAD_VALUE: int = pad_value
 
     @override
     def __getitem__(self, idx: int) -> tuple[Tensor, ...]:
-        if len(self.y) > 0:
-            return cast(tuple[Tensor, Tensor], (Tensor(self.df.loc[idx, self.X].values), Tensor(self.df.loc[idx, self.y].values)))
-        return cast(tuple[Tensor], (Tensorrself.df.loc[idx, self.X].values, )))
+         
+        x = Tensor(self.df.loc[idx, self.X])
+       
+        if self.PAD & x.size(0) < self.MAX_LEN:
+            x = nn.functional.pad(x, (0, self.MAX_LEN - x.size(0)), value=self.PAD_VALUE)
+        if self.TRUNCATE & x.size(0) > self.MAX_LEN:
+            x = x[:self.MAX_LEN]
+        
+        if self.Y is not None:
+            y = Tensor(self.df.loc[idx, self.Y])
+            return (x, y)
+        
+        return (x,)
