169dead16d9a081afc2682fefe5e68984d309ac8
felix <felix.noble@neuro-genesis.uk>
2025-12-21 13:58:34 +0000

    type hints

--- src/models/registry.py
+++ src/models/registry.py
@@ -3,30 +3,30 @@ import importlib
 import inspect
 from pathlib import Path
 
-def build_registry() -> dict:
+def build_registry() -> dict[str, type[nn.Module]]:
     current_path = Path(__file__)
     module_names = [str(f.stem) for f in Path(__file__).parent.glob('*.py') if f != current_path]
 
-    registry = {}
+    registry: dict[str, type[nn.Module]] = {}
     for module_name in module_names:
         module = importlib.import_module(str(module_name))
 
         components = [clss for name,clss in inspect.getmembers(module, inspect.isclass) if hasattr(clss, 'MODEL_NAME')]
         if len(components) > 1:
-            raise ValueError (f'Found two classes in {module_name} Only one class per "model" module expected')
+            raise ValueError (f'Found two models in {module_name}, only one model class per module (file) expected')
         if not components:
-            raise ValueError(f"No valid classes found in '{module_name}', ensure all model classes have 'MODEL_NAME' attribute")
+            raise ValueError(f"No valid classes found in '{module_name}', ensure model classes have 'MODEL_NAME' attribute")
         model = components[0]
         model_name = getattr(model, 'MODEL_NAME')
         
         registry[model_name] = model
     
-        return registry
+    return registry
 
 def get_model(model_name: str) -> type[nn.Module]:
     model_name = model_name.lower()
     alias_registry = {
-        'r_rnn' : ['real_rnn', 'real_recurrentnn']
+        'r_rnn' : ['real_rnn', 'real_recurrent']
     }
 
     for name, alts in alias_registry.items():
@@ -34,12 +34,10 @@ def get_model(model_name: str) -> type[nn.Module]:
             model_name = name
             break
 
-    registry: dict = build_registry()
+    registry: dict[str, type[nn.Module]] = build_registry()
     if model_name not in registry.keys():
-        raise KeyError(f'Model "{model_name}" name not registered, check model registry')
+        raise KeyError(f'Model "{model_name}" name not found. Available models: [ {", ".join(registry.keys())} ]')
     model = registry[model_name]
-    return model 
+    # TODO: check model config with pyright here
 
-if __name__ == '__main__':
-    model=get_model('r_rnn')
-    print(model)
+    return model 
