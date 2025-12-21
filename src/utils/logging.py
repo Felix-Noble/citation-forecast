1882962f77612dd005b6f48252889571cc476fae
felix <felix.noble@neuro-genesis.uk>
2025-12-21 19:22:05 +0000

    removed uneccesary formatter

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -20,16 +20,13 @@ def setup_logger(logger: logging.Logger, config: LogConfig = LogConfig('ERROR', 
     except ValueError as e:
         raise ValueError(f"{e} - change config value")
     file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_formatter = logging.Formatter('%(name)s - %(message)s')
 
     file_handler = logging.FileHandler(Path("logs") / f"{logger.name}.log")
     file_handler.setLevel(level_file)
     file_handler.setFormatter(file_formatter)
 
-    #console_handler = logging.StreamHandler(sys.stdout)
     console_handler = RichHandler(markup=True)
     console_handler.setLevel(level_console) # Log everything to the console for development.
-    console_handler.setFormatter(console_formatter)
 
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
