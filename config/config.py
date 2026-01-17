af3d1431b6c4c5d2d5f659228d82549fa7c20d67
felix <felix.noble@neuro-genesis.uk>
2026-01-17 12:32:54 +0000

    fixed out device

--- config/config.py
+++ config/config.py
@@ -11,7 +11,7 @@ class ModelConfig:
     eos_token: int = 200_002
     embed_dim: int = 128
     attention_dim: int = 128
-    n_layers: int = 2
+    n_layers: int = 4
     n_out: int = 3
 
 Loss_fn = torch.nn.CrossEntropyLoss
@@ -19,10 +19,10 @@ Optimizer = torch.optim.AdamW
 
 @dataclass(frozen=True)
 class TrainConfig:
-    epochs: int = 100
-    batch_size: int = 96
-    opttim_step_interval: int = 32 # n batches until optimizer steps
-    lr: float = 3e-4 
+    epochs: int = 50
+    batch_size: int = 256
+    opttim_step_interval: int = 10 # n batches until optimizer steps
+    lr: float = 1e-3 
     weight_decay: float = 0.9
     loss_fn: str = Loss_fn.__name__
     optimizer: str = Optimizer.__name__
@@ -30,7 +30,7 @@ class TrainConfig:
     checkpoint_interval: int = 4
     shuffle: bool = True
     sample: bool = False
-    mat_mul_precision: str = 'medium'
+    mat_mul_precision: str = 'high'
 
 @dataclass(frozen=True)
 class DataConfig:
@@ -38,9 +38,9 @@ class DataConfig:
     staged: Path = Path('/home/fnoble/data/staged/')
     max_len: int = 300
     train_start: datetime = datetime(2000, 1, 1)
-    train_end: datetime = datetime(2001, 1, 1)
-    test_start: datetime = datetime(2001, 1, 1)
-    test_end: datetime = datetime(2002, 1, 1)
+    train_end: datetime = datetime(2000, 6, 1)
+    test_start: datetime = datetime(2000, 6, 1)
+    test_end: datetime = datetime(2001, 1, 1)
 
 @dataclass(frozen=True)
 class LogConfig:
