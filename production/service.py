060ff81f6a0ad029fdf64bf658206a05c0648b3f
felix <felix.noble@neuro-genesis.uk>
2026-02-11 22:34:02 +0000

    Web EndPoint service - added web endpoint to hand off requests - auto batching for process_requests - REMAINING: endpoint does'nt differentite error returns

--- production/service.py
+++ production/service.py
@@ -1,8 +1,6 @@
-import sys
 import os
 import modal
 import torch
-
 app = modal.App('citescout-general')
 
 tokenizer_volume = modal.Volume.from_name('tokenizer-gpt-oss-120b', create_if_missing=True)
@@ -10,12 +8,14 @@ weights_volume = modal.Volume.from_name('general-weights', create_if_missing=Tru
 
 image = (
     modal.Image.debian_slim(python_version='3.13')
-    .uv_pip_install("torch<3", "transformers", "pydantic")
-    .add_local_dir('./models/general/model/', remote_path='/code/model')
+    .pip_install("torch<3", "transformers", "pydantic", "fastapi")
+    .add_local_python_source('models')
 )
 
 @app.cls(
     image=image,
+    max_containers=1,
+    timeout=120,
     gpu="T4",
     volumes={
         '/weights': weights_volume,
@@ -27,16 +27,17 @@ class InferenceWrapper:
     @modal.enter()
     def setup(self):
         # Model
-        sys.path.append('/code')
+        
         #sys.path.append('./models/general/model')
-        from model import Model, config
+        from models.general.model import Model, config
+
+        self.device=torch.device('cuda')
+        self.max_len = config.data.max_len
 
         self.model = Model(config.model, torch.device('cuda'), torch.float32)
-
-        self.max_len = config.data.max_len
         self.model.eval()
         # Weights
-        self.model.load_state_dict(torch.load('/weights/weights.pt', map_location='cuda', weights_only=True))
+        self.model.load_state_dict(torch.load('/weights/weights.pt', weights_only=True))
 
         # Tokenizer
         from transformers import AutoTokenizer
@@ -46,30 +47,50 @@ class InferenceWrapper:
             self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
         else:
             self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-            self.tokenizer.pad_token = 0
+            self.tokenizer.pad_token_id = 0
             self.tokenizer.save_pretrained(tokenizer_path)
             tokenizer_volume.commit() 
 
-    @modal.method()
-    def process_request(self, prompt: str):
+    @modal.batched(max_batch_size=4, wait_ms=500)
+    def process_request(self, prompt: list[str] | list[list[str]]):
+        if isinstance(prompt[0], list):
+            unwrapped = []
+            for ls in prompt:
+                unwrapped += ls
+            prompt = unwrapped
         try: 
-            prompt = self.tokenizer.bos_token + prompt + self.tokenizer.eos_token
-
-            tokens, mask = self.tokenizer(
+            token_out = self.tokenizer(
                 prompt, 
+                add_special_tokens=True,
                 return_tensors="pt",
                 max_len=self.max_len,
                 padding=True,
+                truncate=True,
                 padding_side='right',
-
             )
 
-            mask = mask.bool().unsqueeze(0).expand(self.max_len, -1)
+            tokens = token_out['input_ids'].to(self.device)
+            mask = token_out['attention_mask'].to(self.device)
+            mask = mask.bool().unsqueeze(1).expand(-1, mask.size(-1), -1)
 
             with torch.inference_mode():
                 logits = self.model(tokens, mask)
                 probs = torch.softmax(logits, dim=-1)
-                return probs.cpu().tolist()
+
+            return probs.cpu().tolist()
 
         except Exception as e:
-            return {'status': 'Error', 'message': str(e)}
+            return [str(e) for _ in range(len(prompt))]
+
+@app.function(
+    image=image,
+    max_containers=1,
+    timeout=120,
+)
+@modal.fastapi_endpoint(method="POST", requires_proxy_auth=True)
+async def predict(data: dict):
+    # This creates an instance of the class and calls the batched method
+    # Modal automatically groups these calls into 'handle_batch'
+    instance = InferenceWrapper()
+    probs = instance.process_request.remote([data["prompt"]])
+    return {"probs": probs}
