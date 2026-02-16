a532e035c227002fb251975916b1118dc9dd3d5f
felix noble <felix.noble@neuro-genesis.uk>
2026-02-16 21:29:32 +0100

    rename: dataset > datasets

--- src/builders/__init__.py
+++ src/builders/__init__.py
@@ -1,14 +1,16 @@
 # src/builders/__init__.py 
 
-from .build_dataset import build_ordinal_dataset
+from .build_datasets import build_ordinal_datasets
 from .build_dataloader import build_dataloader
 from .build_tracker import build_tracker_params
 from .build_lr_scheduler import build_lr_scheduler
 from .build_progress_bars import build_progress_bars
+from .build_model import build_model
 
-__all__ = [ 'build_ordinal_dataset',
+__all__ = [ 'build_ordinal_datasets',
            'build_dataloader',
            'build_tracker_params',
            'build_lr_scheduler',
-           'build_progress_bars'
+           'build_progress_bars',
+           'build_model',
            ]
