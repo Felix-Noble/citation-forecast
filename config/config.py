9476f063d0313d4597290c50ef3b74e19651d230
felix noble <felix.noble@neuro-genesis.uk>
2026-02-17 23:37:19 +0100

    feat: h_r_smooth model
    - add heirarchical recurrent model with one hot class gaussian smoothing
    - add config driven control over which layers use recurrent process

--- config/config.py
+++ config/config.py
@@ -2,41 +2,45 @@ import torch
 from datetime import datetime
 from dataclasses import dataclass
 
-top_k = (300 for _ in range(8))
-top_k = (*top_k, 256, 128, 64, 32, 16, 8, 4)
+top_k = tuple(300 for _ in range(8))
+top_k = (*top_k, 128, 128, 128, 128)
 n_layers = len(top_k)
 selector_heads = tuple(4 for _ in range(n_layers))
 process_heads = tuple(4 for _ in range(n_layers))
+r_layers = tuple(False if i not in (3,10) else True for i in range(n_layers))
+
 @dataclass(frozen=True)
 class LossConfig:
-    beta: float = 0.1
+    beta: float = 1
 
 @dataclass(frozen=True)
 class ModelConfig:
-    model_name: str = 'h_attn_single'
+    model_name: str = 'h_r_smooth'
     vocab_size: int = 201_088
     pad_token_id: int = 0
     dtype: torch.dtype =  torch.float32
     top_k: tuple[int, ...] = top_k
+    r_layers: tuple[bool, ...] = r_layers
     selector_heads: tuple[int, ...] = selector_heads
     process_heads: tuple[int, ...] = process_heads
     n_layers: int = n_layers
     max_len: int = 300
-    embed_dim: int = 512
-    hidden_dim: int = 512
-    n_out: int = 3
+    embed_dim: int = 8
+    hidden_dim: int = 8
+    n_out: int = 6
     dropout: float = 0.05
 
 @dataclass(frozen=True)
 class TrainConfig:
     epochs: int = 200
-    batch_size: int = 3
+    batch_size: int = 12
     grad_accumulation_steps: int = 50 # n batches until optimizer steps
-    lr: float = 1e-2 
+    lr: float = 1e-4 
     lr_milestones: tuple[int, ...] = (40, 80)
     weight_decay: float = 0.9
-    loss_fn: str = 'CrossEntropyLoss'
     optimizer: str = 'AdamW'
+    loss_fn: str = 'WassersteinEntropyLoss'
+    loss: LossConfig = LossConfig()
     eval_interval: int = 5
     checkpoint_interval: int = 10
     shuffle: bool = False
