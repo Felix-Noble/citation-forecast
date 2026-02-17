e6b6237bbf12c7b3ddafaff8e363d8d73687c1f6
felix noble <felix.noble@neuro-genesis.uk>
2026-02-17 13:28:05 +0100

    refractor & tidy: replace imports, fixed attr name
    - move imports to general import all locals func
    - fix model config attr name to refect transformers.tokenizer naming
    - fix eval training local import issue

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,11 +1,5 @@
 from .main import app as train_app
-from .tracking import ClassificationTracker, log_params
-from .eval.eval_model import eval
-from .callbacks import isnan_async
 
 __all__ = [
         'train_app',
-        'ClassificationTracker', 'log_params',
-        'eval',
-        'isnan_async'
         ]
