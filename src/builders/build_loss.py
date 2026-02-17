17e0ba9ce4022fd588e891ed64e67ca492c9affd
felix noble <felix.noble@neuro-genesis.uk>
2026-02-17 21:26:23 +0100

    add docs & safety check

--- src/builders/build_loss.py
+++ src/builders/build_loss.py
@@ -3,4 +3,6 @@ from src.training.losses import loss_registry
 import torch
 
 def build_loss(config: Config = config) -> torch.nn.modules.loss._Loss:
+    """ Builds loss obj, expects PyTorch style (callable class)"""
+    assert config.train.loss_fn in loss_registry.keys, f'{config.train.loss_fn} not registered, available: {", ".join(loss_registry.keys)}'
     return loss_registry[config.train.loss_fn]()
