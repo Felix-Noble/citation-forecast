6c7c2388aec8a22053c0bfc06637b20606468998
felix <felix.noble@neuro-genesis.uk>
2026-03-06 16:29:23 +0000

    initial commit

--- src/data/preprocess/stage_data.py
+++ src/data/preprocess/stage_data.py
@@ -1,5 +1,10 @@
+import polars as pl
+import os
+import gc
 # pyright: basic
 # pyright: reportAttributeAccessIssue=false, reportPrivateImportUsage=false
+
+os.environ['POLARS_MAX_THREADS'] = '32'
 
 def main(
     raw: str,
@@ -7,52 +12,53 @@ def main(
     start_date: int,
     end_date: int,
     field_id: int,
-    drop_na_cols: list[str],
+    drop_na_cols: list[str] = [],
+    num_files:int = 64,
 ) -> None:
     
-    client = Client(n_workers=4, threads_per_worker=8, memory_limit=f'{58/4}GB')
+    df = pl.scan_parquet(raw, extra_columns='ignore')
+    #df = df.filter((pl.col('publication_date_int') >= start_date) & (pl.col('publication_date_int') < end_date))
+    #df = df.filter(pl.col('field_id') == field_id)
+    #df = df.drop_nulls(subset=drop_na_cols)
 
-    ddf = dd.read_parquet(
-        raw,
-        engine='fastparquet'
-    )
-     
-    ddf = ddf[(ddf['publication_date_int'] >= start_date) & (ddf['publication_date_int'] < end_date) & (ddf['field_id'] == field_id)] 
-
-    if drop_na_cols:
-        ddf = ddf.dropna(subset=drop_na_cols)
-
-    ddf = ddf.repartition(npartitions=64)
     print('writing parquet')
-    write_op = ddf.to_parquet(
-        out,
-        engine='fastparquet',
-        compute=False,
-        overwrite=True,
-    )
-    progress(client.compute(write_op))
+    i = 0
+    n = 1_000_000
+    while True:
+        
+        df_part = df.slice(i*n, n)#.collect(engine='streaming')
+        len = df_part.select(pl.len()).collect(engine='streaming').item()
+        if len < 1:
+            break
+        df_part.sink_parquet(
+            f'{out}/part{i}.parquet',
+            statistics=True,
+            compression='zstd',
+            compression_level=8,
+        )
+        print(f'finished writing part{i}')
+        i += 1
     print('finished')
 
 if __name__ == '__main__':
-    import dask.dataframe as dd
-    from dask.distributed import Client, progress
-    from config.config import config
+    import os
+    import sys
+    sys.path.append('/home/fnoble/projects/citation-forecast/')
+    from config import env
     import datetime
     start_date = datetime.date(1960, 1, 1).toordinal()
-    end_date = datetime.date(2020, 1, 1).toordinal()
+    end_date = datetime.date(2026, 1, 1).toordinal()
     field_id = 32
     
-    if config.data.staged.exists():
-        selection = input('output folder exists, overwrite? Y/n : ')
-        if not selection.lower() == 'y':
-            quit()
+    field_name = 'all'
+    os.makedirs(env.STAGED_LOC / field_name, exist_ok=True)
 
     print('starting main body')
     main(
-        str(config.data.raw),
-        str(config.data.staged / 'psychology'),
+        str(env.RAW_LOC),
+        str(env.STAGED_LOC / field_name),
         start_date,
         end_date,
         field_id,
-        ['id', 'abstract'],
+        ['id', 'title'],
     )
