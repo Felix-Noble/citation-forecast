3743ae858e7c6eaf7974e2db89e0f9059abc75b6
felix noble <felix.noble@neuro-genesis.uk>
2026-03-17 14:27:41 +0100

    feat: preprocess app
    - centralised core preprocess app arg parse & logic
    - line/file exporting & progress display
    - cleaning optional per column
    - tokenising optional per column
    - datetime, lang, & field filtering via cli arg

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from src import train_app
+from src.apps import preprocess
 import typer
 
 app = typer.Typer(pretty_exceptions_enable=False)
 app.add_typer(train_app, name='train')
+app.add_typer(preprocess, name='preprocess')
 
 if __name__ == '__main__':
     app()
