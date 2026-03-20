b47315be745a0ace2b02ea57a72ca94d047a0fc9
felix noble <felix.noble@neuro-genesis.uk>
2026-03-20 13:07:58 +0100

    feat: binary classification
    - add binary categorical dataset to classify > 0 citations
    - generalised build dataset logic, dataset selected in datasets init
    - add pyrightconfig to gitignore

--- src/builders/build_datasets.py
+++ src/builders/build_datasets.py
@@ -1,6 +1,38 @@
 # src/builders/build_dataset_ordinal.py
-from src.data import OrdinalDataset
+from src.data.datasets import OrdinalDataset, BinaryCategoricalDataset
 from config import Config, config, env
+
+def build_binary_datasets(
+        dataset: str,
+        dry_run: bool, 
+        config: Config = config,
+        ):
+
+    train_dataset = BinaryCategoricalDataset(
+        data_path=str(env.STAGED_LOC / dataset),
+        X='abstract_tokens',
+        y='citation_normalized_percentile',
+        t_start=config.train.train_start.toordinal(),
+        t_end=config.train.train_end.toordinal(),
+        config=config,
+        pad=True,
+        return_mask=True,
+        dry_run=dry_run
+    )
+
+    test_dataset = BinaryCategoricalDataset(
+        data_path=str(env.STAGED_LOC / dataset),
+        X='abstract_tokens',
+        y='citation_normalized_percentile',
+        t_start=config.train.test_start.toordinal(),
+        t_end=config.train.test_end.toordinal(),
+        config=config,
+        pad=True,
+        return_mask=True,
+        dry_run=dry_run
+    )
+    
+    return train_dataset, test_dataset
 
 def build_ordinal_datasets(
         dataset: str,
---
+++ src/data/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .ordinal_dataset import OrdinalDataset
+from .binary_categorial_dataset import BinaryCategoricalDataset
+
+__all__ = [
+        'OrdinalDataset',
+        'BinaryCategoricalDataset',
+        ]
