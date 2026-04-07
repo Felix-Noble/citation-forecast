ef09608b87915daa6eaeb10b08f25c71ccd234cf
felix noble <felix.noble@neuro-genesis.uk>
2026-04-07 15:45:02 +0200

    refactor: simplified tracker init
    - single func for building tracker params
    - loop (train/eval) specific tracker build funcs

--- src/builders/__init__.py
+++ src/builders/__init__.py
@@ -2,7 +2,7 @@
 
 from .build_datasets import build_ordinal_datasets, build_binary_datasets
 from .build_dataloader import build_dataloader
-from .build_tracker import build_tracker
+from .build_tracker import build_train_tracker, build_eval_tracker
 from .build_lr_scheduler import build_lr_scheduler
 from .build_progress_bars import build_progress_bars, build_eval_example_progress, build_epoch_progress
 from .build_model import build_model
@@ -15,7 +15,7 @@ def build_datasets(**kwargs):
 __all__ = [ 
            'build_datasets',
            'build_dataloader',
-           'build_tracker',
+           'build_train_tracker', 'build_eval_tracker',
            'build_lr_scheduler',
            'build_progress_bars', 'build_eval_example_progress', 'build_epoch_progress',
            'build_model',
--- src/builders/build_tracker.py
+++ src/builders/build_tracker.py
@@ -2,15 +2,39 @@ from config import Config, config
 from src.training.tracking import ClassificationTracker
 from src.training.tracking.classification_tracker import StoreParams
 import torch
-
-def build_tracker(
+def build_eval_tracker(
         device: torch.device,
         dtype: torch.dtype,
         config: Config = config
-                  ):
+                  ) -> ClassificationTracker:
+       param_tuple = (
+            build_tracker_params(name='eval_logits', device=device),
+            build_tracker_params(name='eval_y', device=torch.device('cpu')),
+            build_tracker_params(name='eval_loss', device=device),
+               ) 
+       metric_tracker = ClassificationTracker(
+                param_tuple,
+                dtype=dtype,
+                device=device,
+                buffer=False,
+                )
+       return metric_tracker
 
+def build_train_tracker(
+        device: torch.device,
+        dtype: torch.dtype,
+        config: Config = config
+                  ) -> ClassificationTracker:
+    param_tuple = (
+            build_tracker_params(name='train_logits', device=device),
+            build_tracker_params(name='train_y', device=torch.device('cpu')),
+            build_tracker_params(name='train_loss', device=device),
+            build_tracker_params(name='test_logits', device=device),
+            build_tracker_params(name='test_y', device=torch.device('cpu')),
+            build_tracker_params(name='test_loss', device=device),
+            )
     metric_tracker = ClassificationTracker(
-        build_tracker_params(device=device, config=config),
+        param_tuple,
         dtype=dtype,
         device=device,
         buffer=False,
@@ -18,61 +42,16 @@ def build_tracker(
     return metric_tracker
 
 def build_tracker_params(
+        name: str,
         device: torch.device,
         config: Config = config, 
         ):
-    gpu = device
-    cpu = torch.device('cpu')
-    train_logits = StoreParams(
-        'train_logits',
+    params = StoreParams(
+        name,
         batch_shape=(config.train.batch_size, 5),
         buffer_size=100,
-        buffer_device=gpu,
+        buffer_device=device,
         max_store=-1,
         n_examples=-1,
     )
-    train_y = StoreParams(
-        'train_y',
-        batch_shape=(config.train.batch_size, ),
-        buffer_size=100,
-        buffer_device=cpu,
-        max_store=-1,
-        n_examples=-1,
-    )
-
-    train_loss = StoreParams(
-        'train_loss',
-        batch_shape=(config.train.batch_size, ),
-        buffer_size=100,
-        buffer_device=gpu,
-        max_store=-1,
-        n_examples=-1,
-    )
-
-    test_logits = StoreParams(
-        'test_logits',
-        batch_shape=(config.train.batch_size, 5),
-        buffer_size=100,
-        buffer_device=gpu,
-        max_store=-1,
-        n_examples=-1,
-    )
-
-    test_y = StoreParams(
-        'test_y',
-        batch_shape=(config.train.batch_size, ),
-        buffer_size=100,
-        buffer_device=cpu,
-        max_store=-1,
-        n_examples=-1,
-    )
-
-    test_loss = StoreParams(
-        'test_loss',
-        batch_shape=(config.train.batch_size, ),
-        buffer_size=100,
-        buffer_device=gpu,
-        max_store=-1,
-        n_examples=-1,
-    )
-    return train_logits, train_y, train_loss, test_logits, test_y, test_loss
+    return params
