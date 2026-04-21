9b65a625a24b97a9dab408082effd649943c42cf
felix <felix.noble@neuro-genesis.uk>
2026-04-21 21:22:29 +0000

    update safety check

--- config/__init__.py
+++ config/__init__.py
@@ -11,9 +11,9 @@ __all__ = [
         ]
 
 # Safety checks
-if config.train.sample:
+if config.train.sample is not None:
     assert config.train.sample % config.train.batch_size  == 0, 'sample % batch_size must == 0'
 
-if config.train.sample and config.train.shuffle:
+if config.train.sample is not None and config.train.shuffle:
     raise ValueError('trian.shuffle and train.sample cannot both be true')
 
