6a7bf462e4589b20a3c90c081b2cbfef79d55500
felix <felix.noble@neuro-genesis.uk>
2026-05-12 11:35:24 +0000

    fix: dataset name & n examps formatting

--- src/training/tracking/log_params.py
+++ src/training/tracking/log_params.py
@@ -4,9 +4,7 @@ import mlflow
 from ._get_scheduler_params import _get_scheduler_params
 
 def log_params(
-        train_dataset_name: str,
         train_dataset,
-        test_dataset_name: str,
         test_dataset,
         scheduler,
         config: Config = config
@@ -17,14 +15,10 @@ def log_params(
         mod_params = {f'{k}-{sk}': val for sk, val in v.__dict__.items()}
         param_dict.update(mod_params)
 
-    param_dict['train_dataset'] = {
-            'name': train_dataset_name,
-            'n_examps': f'{len(train_dataset):,}'
-            }
-    param_dict['test_dataset'] = {
-            'name': test_dataset_name,
-            'n_examps': f'{len(test_dataset):,}'
-            }
+    param_dict['train_dataset'] = config.train.dataset
+    param_dict['test_dataset'] = config.train.dataset
+    param_dict['train_n_examps'] = len(train_dataset)
+    param_dict['test_n_examps'] = len(test_dataset)
 
     scheduler_params = _get_scheduler_params(scheduler)
     param_dict.update({f'scheduler-{k}':v for k,v in scheduler_params.items()})
