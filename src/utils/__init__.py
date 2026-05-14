3c6593e97d394c3924b70891b286a951d06a9b95
felix <felix.noble@neuro-genesis.uk>
2026-05-14 14:45:10 +0000

    feat: export parq util func

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,11 @@
 from .registry import Registry
+from .export_parquet import export_parquet
 from .logging import setup_logger
 from .import_all_local import import_all_local
  
 __all__ = [
         'Registry',
         'setup_logger',
-        'import_all_local'
+        'export_parquet',
+        'import_all_local',
         ]
