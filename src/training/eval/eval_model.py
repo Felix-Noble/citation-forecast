ff9b68a18aec4f280a2137d7623d79e67ad69acd
felix noble <felix.noble@neuro-genesis.uk>
2026-06-12 10:01:47 +0200

    fix: train loop batch parsing

--- src/training/eval/eval_model.py
+++ src/training/eval/eval_model.py
@@ -1,5 +1,6 @@
 # src/training/eval/eval_model.py 
 from ..tracking import BinaryClassificationTracker 
+from src.data.datasets.polars_dataset import Output
 from config import config
 import torch
 import torch.nn as nn
@@ -20,25 +21,24 @@ def eval_model(
 
     model.eval()
     with torch.no_grad():
-        for batch_i, batch in enumerate(dataloader):
+        for batch_i, batch_dict in enumerate(dataloader):
+            batch = Output(**batch_dict)
             y = batch.y
             metric_tracker.process_values((y,), ('test_y',))
             with stream_context:
-                X = batch.X.to(device, non_blocking=True)
+                x = batch.x.to(device, non_blocking=True)
                 y = y.to(device, non_blocking=True)
                 mask = batch.mask.to(device, non_blocking=True)
             stream_sync()
 
-            logits, probs, sigma = model(X, mask)
-            loss = loss_fn(logits=logits, probs=probs, sigma=sigma, target=y)
+            logits, probs = model(x, mask)
+            loss = loss_fn(logits=logits, probs=probs, target=y)
 
             loss_cpu = loss.detach().cpu().item()
-            sigma_cpu = torch.mean(sigma.detach()).item()
 
-            metric_tracker.log_metric('test_loss', loss_cpu, X.shape[0])
-            metric_tracker.log_metric('test_sigma', sigma_cpu, X.shape[0])
-            if batch.id is not None:
-                metric_tracker.log_metric('test_id', batch.id, X.shape[0])
+            metric_tracker.log_metric('test_loss', loss_cpu, x.shape[0])
+            if not torch.any(torch.isnan(batch.id)):
+                metric_tracker.log_metric('test_id', batch.id, x.shape[0])
 
             metric_tracker.process_values((logits.detach(), probs.detach()), ('test_logits', 'test_probs'))
             example_progress.update(examples_done, advance=config.train.batch_size)
