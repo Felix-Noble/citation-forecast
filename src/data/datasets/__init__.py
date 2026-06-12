8198d071f9d3ffd72b9410d053be22c2b9170c5a
felix noble <felix.noble@neuro-genesis.uk>
2026-06-12 10:04:39 +0200

    fix: init generative pretrained dataset

--- src/data/datasets/__init__.py
+++ src/data/datasets/__init__.py
@@ -2,6 +2,7 @@ from ._registry import dataset_registry
 from .ordinal_dataset import OrdinalDataset
 from .binary_categorial_dataset import BinaryCategoricalDataset
 from .binary_threshold_dataset import BinaryThresholdDataset
+from .generative_pretrain_dataset import GenerativePretrainDataset
 
 __all__ = [
         'dataset_registry',
