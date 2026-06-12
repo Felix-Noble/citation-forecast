920c982e3acbf58b9ba6173da01515f60c8d8c6b
felix noble <felix.noble@neuro-genesis.uk>
2026-06-12 10:02:02 +0200

    fix: CELoss input parsing

--- src/training/losses/cross_entropy.py
+++ src/training/losses/cross_entropy.py
@@ -1,6 +1,21 @@
 from ._registry import loss_registry
+from typing import override
+from torch import Tensor
 import torch.nn as nn
 
 @loss_registry('CrossEntropyLoss')
 class CrossEntropyLoss(nn.CrossEntropyLoss):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.torch_ce_loss = nn.CrossEntropyLoss()
+
+    @override
+    def __call__(
+            self, 
+            probs: Tensor,
+            target: Tensor,
+            **kwargs
+            ) -> Tensor:
+        print('probs', probs.shape) 
+        print('target', target.shape) 
+        return self.torch_ce_loss(input=probs, target=target.squeeze(-1))
