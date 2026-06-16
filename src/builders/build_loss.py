2405b751673b3ba30f305f921b648f8bc7bb66c3
felix <felix.noble@neuro-genesis.uk>
2026-06-16 12:15:21 +0000

    feat: config parsing to loss builder

--- src/builders/build_loss.py
+++ src/builders/build_loss.py
@@ -1,8 +1,8 @@
-from config import Config, config
+from config import Config
 from src.training.losses import loss_registry
 import torch
 
-def build_loss(config: Config = config) -> torch.nn.modules.loss._Loss:
+def build_loss(config: Config) -> torch.nn.modules.loss._Loss:
     """ Builds loss obj, expects PyTorch style (callable class)"""
     assert config.train.loss_fn in loss_registry.keys, f'{config.train.loss_fn} not registered, available: {", ".join(loss_registry.keys)}'
-    return loss_registry[config.train.loss_fn]()
+    return loss_registry[config.train.loss_fn](config)
