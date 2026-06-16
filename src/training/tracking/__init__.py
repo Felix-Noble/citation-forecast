c146854203fe44f3ef11b3af66ea2be9d2612edb
felix <felix.noble@neuro-genesis.uk>
2026-06-16 12:18:00 +0000

    feat: calc metrics API export

--- src/training/tracking/__init__.py
+++ src/training/tracking/__init__.py
@@ -2,10 +2,12 @@ from .metric_tracker import MetricTracker
 from .binary_classification_tracker import BinaryClassificationTracker
 from .log_params import log_params
 from .log_lrs import log_lrs
+from .calc_metrics import calc_metrics
 
 __all__ = [
         'MetricTracker',
         'BinaryClassificationTracker',
         'log_params',
-        'log_lrs'
+        'log_lrs',
+        'calc_metrics', 
         ]
