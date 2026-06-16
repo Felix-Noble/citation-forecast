e63eafc4752153e3020ea70628cfa46bc69e2c47
felix <felix.noble@neuro-genesis.uk>
2026-06-16 12:17:30 +0000

    feat: calc metrics function

---
+++ src/training/tracking/calc_metrics.py
@@ -0,0 +1,13 @@
+from config import Config
+import torch
+from torch import Tensor
+from ..losses import norm_entropy_loss
+
+def calc_metrics(config: Config, probs: Tensor, targets: Tensor) -> dict[str, Tensor]:
+    metrics: dict[str, Tensor] = {}
+    metrics['entropy'] = norm_entropy_loss(probs)
+     
+    target_ont_hot: Tensor = torch.nn.functional.one_hot(targets, num_classes=config.model.n_out)
+    metrics['mae'] = torch.sum(torch.abs(target_ont_hot - probs)) / (sum(probs.shape[:2]) * config.model.n_out) # normalised mean absolute error
+
+    return metrics
