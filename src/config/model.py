4b1a5a42bd5aa7efbb8a69ecd96642b600c95eda
felix noble <felix.noble@neuro-genesis.uk>
2026-06-20 20:59:26 +0200

    edit config

--- src/config/model.py
+++ src/config/model.py
@@ -2,19 +2,15 @@ import torch
 
 import models
 
-clss = models.Abstractor
+clss = models.TransformerClass
 model = clss.config(
     vocab_size=201_088,
     pad_token_id=19999,
     dtype=torch.float32,
-    process_heads=1,
+    n_heads=8,
     n_layers=8,
-    n_forward=1,
-    n_abstractions=1,
-    abstraction_heads=2,
-    abstracted_seq_len=100,
-    max_len=100,
-    max_len_eval=100,
+    # n_abstractions=1,
+    max_len=100,  # TODO: move to data config
     embed_dim=8,
     hidden_dim=8,
     n_out=1,
