8cb527d49cc3843096418e43cdb2ef973201759b
felix <felix.noble@neuro-genesis.uk>
2026-06-21 13:24:07 +0000

    fix: old model export

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,9 @@
 from .abstractor import Abstractor
+from .transformerClass import TransformerClass
+from .hr_ahead_binary import HR_AHEAD_BINARY
 
 __all__ = [
+        'HR_AHEAD_BINARY',
     "Abstractor",
+    "TransformerClass",
 ]
