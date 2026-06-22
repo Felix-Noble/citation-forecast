27fab8535d387e35448f772536bb6eb617df706a
felix <felix.noble@neuro-genesis.uk>
2026-06-22 11:11:08 +0000

    feat: log regression dataset

--- src/data/datasets/__init__.py
+++ src/data/datasets/__init__.py
@@ -3,10 +3,12 @@ from .binary_threshold_dataset import BinaryThresholdDataset
 from .generative_pretrain_dataset import GenerativePretrainDataset
 from .generative_pretrain_dataset2 import GenerativePretrainDataset2
 from .ordinal_dataset import OrdinalDataset
+from .log_regress_dataset import LogRegressDataset
 from .polars_dataset import PolarsDataset
 
 __all__ = [
     "PolarsDataset",
+    'LogRegressDataset',
     "OrdinalDataset",
     "BinaryCategoricalDataset",
     "BinaryThresholdDataset",
---
+++ src/data/datasets/log_regress_dataset.py
@@ -0,0 +1,32 @@
+from logging import getLogger
+from typing import override
+
+import torch
+from torch import Tensor
+
+from utils.logging import setup_logger
+
+from .polars_dataset import Env, PolarsDataset, PolarsDatasetConfig
+
+logger = getLogger(__name__)
+_ = setup_logger(logger)
+
+
+class LogRegressDatasetConfig(PolarsDatasetConfig):
+    pass
+
+class LogRegressDataset(PolarsDataset):
+    "Binary dataset with custom inclusive threshold (theta) for boundary between 0/1 class"
+
+    config: type[LogRegressDatasetConfig] = LogRegressDatasetConfig
+
+    def __init__(
+        self,
+        config: LogRegressDatasetConfig,
+        env: Env,
+    ):
+        super().__init__(config, env)
+
+    @override
+    def _format_y(self, y: Tensor) -> Tensor:
+        return torch.tensor(torch.log(y + 1), dtype=torch.float32)
