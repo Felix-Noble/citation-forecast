95deaaf90eb50e511afe8821389b5ea23e8eaef6
felix noble <felix.noble@neuro-genesis.uk>
2026-06-22 13:16:43 +0200

    fix: models init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,11 @@
 from .abstractor import Abstractor
 from .transformerClass import TransformerClass
 from .hr_ahead_binary import HR_AHEAD_BINARY
+from .transformer_var_reg import TransformerVarianceRegressor
 
 __all__ = [
         'HR_AHEAD_BINARY',
+        'TransformerVarianceRegressor',
     "Abstractor",
     "TransformerClass",
 ]
