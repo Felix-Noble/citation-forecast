020ab7c5ec7b9effe3cd3e5cf683ca56657f8bce
felix <felix.noble@neuro-genesis.uk>
2026-06-22 11:12:14 +0000

    feat: regression trainer

--- src/training/trainers/__init__.py
+++ src/training/trainers/__init__.py
@@ -1,8 +1,10 @@
 from .base_trainer import BaseTrainer, Batch, TrainerConfig, TrainerProtocol
 from .classifier_trainer import ClassifierTrainer
+from .regress_trainer import RegressTrainer
 
 __all__ = [
     "BaseTrainer",
+    'RegressTrainer',
     "TrainerProtocol",
     "TrainerConfig",
     "Batch",
