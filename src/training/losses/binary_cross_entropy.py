f264fc9ef450d8ef59cf1370f8ec7e1d2614a168
felix <felix.noble@neuro-genesis.uk>
2026-06-22 11:11:34 +0000

    fix: functional bce implementation

--- src/training/losses/binary_cross_entropy.py
+++ src/training/losses/binary_cross_entropy.py
@@ -2,6 +2,7 @@ from typing import Protocol, override
 
 import torch
 from torch import Tensor
+from torch.nn.functional import binary_cross_entropy
 from torch.nn import BCELoss
 
 from ._registry import loss_registry
@@ -10,6 +11,7 @@ from .loss_protocol import LossFn
 
 class BCEBatch(Protocol):
     y: Tensor
+    weight: Tensor
 
 
 class BCEOutput(Protocol):
@@ -20,11 +22,12 @@ class BCEOutput(Protocol):
 class BinaryCrossEntropyLoss(BCELoss, LossFn[BCEBatch, BCEOutput]):
     def __init__(self, config):
         super().__init__()
-        self.torch_bce_loss = BCELoss()
+        self.torch_bce_loss = binary_cross_entropy
 
     @override
     def __call__(self, output: BCEOutput, batch: BCEBatch) -> Tensor:
         return self.torch_bce_loss(
             input=output.probs.squeeze(-1),
             target=batch.y.squeeze(-1),
+            weight=batch.weight.squeeze(-1) if not torch.any(torch.isnan(batch.weight)) else None,
         )
